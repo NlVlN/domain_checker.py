c0f11ec9fb5b41e4de1ee74119383ac7853aab10
NlVlN <unknowmartin1@gmail.com>
2018-01-31 00:53:00 +0100

    update domain_checker.py
    Now script will read domain list you want to check from source_list.txt.
    Script will sleep random amount of time if the request limit is exceeded

--- domain_checker.py
+++ domain_checker.py
@@ -1,18 +1,33 @@
 #-*- coding: utf-8 -*-
 import subprocess
 import time
-char = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
+from random import randint
+
+#import domain list to check from 'source_list.txt' file
+with open('source_list.txt') as f:
+    char = f.read().splitlines()
 data = ''
 
-for z in char:
-    for x in char:
-        for c in char:
-            tmp = '%s%s%s.pl' % (z, x, c)
-            bash = str(subprocess.getoutput('whois {0}'.format(tmp)))
+
+for line in char:
+    print('testing {0}'.format(line))
+    while True:
+        bash = str(subprocess.getoutput('whois {0}'.format(line)))
+        # make sure that your limit statement looks identical to the one in the if statemant below 
+        if ('request limit exceeded' in bash):
+            print('\nrequest limit exceeded, sleep few seconds')
+            time.sleep(randint(450, 750))
+        else:
+            #check and modify your statement for unregistered domain
             if ('No information available about domain name' in bash):
-                data += tmp + '\n'
+                data += line + '\n'
                 with open('domain_list.txt', 'w') as f:
                     f.write(data)
+                print('\ndomain found\nadded to domain_list.txt')
+                break
             else:
-                pass
-            time.sleep(37)
+                print('Domain registered')
+                break
+    time.sleep(randint(20, 60))
+
+
